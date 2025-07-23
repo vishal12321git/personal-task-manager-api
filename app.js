0098e3931852805e5dbedcc73cd70e276ff51766
vishal12321git <vishalpandey7594@gmail.com>
2025-07-23 13:17:08 +0530

    changed config for dev

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const {sequelize} = require('./models');
 const cookieParser = require('cookie-parser');
 const authRouter = require('./routes/authRoutes.js');
 const taskRouter = require('./routes/taskRoutes.js');
@@ -16,13 +15,5 @@ app.use(cookieParser());
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/tasks', taskRouter);
 
-app.listen(PORT, async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Database connected successfully.');
-  } catch (err) {
-    console.error('Database connection failed:', err);
-    process.exit(1);
-  }
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.listen(PORT, ()=> console.log(`server running at port ${PORT}`));
+
